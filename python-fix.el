535b0068bc6e0535e777a1a4347ecd5d753fed4e
John <jrciminski@gmail.com>
2018-01-18 12:30:50 -0700

    dump function template; tuneup fix-line

--- python-fix.el
+++ python-fix.el
@@ -26,7 +26,6 @@
                    (condition-case  nil
                        (replace-match (cadr item) nil nil)
                      (error nil))))
-;                 (perform-replace (car item) (cadr item) nil t nil nil)
               '(("( " "(")                      ; no space after (
                  ("\\[ " "[")                   ; no space after [
                  (" \\[" "[")                   ; no space before [
@@ -48,16 +47,51 @@
                  ("\\([^ ]\\)[ ]*\\([<>]\\)[ ]*\\([^ ]\\)" "\\1 \\2 \\3")
                                                 ; single spaces around ==
                  ("\\([^ ]\\)[ ]*==[ ]*\\([^ ]\\)" "\\1 == \\2")
-                 (" \\+ = " " += ")           ; fix  += 
-                 (" \\! = " " != ")           ; fix  != 
-                 ("%(" "% (")                 ; fix  % (
-                 ("popen" "Popen")      ; fix Popen
+                 (" \\+ = " " += ")             ; fix  += 
+                 (" \\! = " " != ")             ; fix  != 
+                 ("%(" "% (")                   ; fix  % (
+                 ("popen" "Popen")              ; fix Popen
                  ("\\b\\(\\w+\\)\\b = \\b\\(\\w+\\)\\b\\([,)]\\)"
-                  "\\1=\\2\\3")             ; no spaces on param assignment
-              ))
-
-
+                  "\\1=\\2\\3")                 ; no spaces on param assignment
+                 ("\\( if\\| while\\| return\\)(" "\\1 (") ))
+                                                ; fix 1 space before (
       ; This must be last.
-      (widen))
-    ))
+      (widen))))
+
+(defun python-helpers-dump-function-template ()
+  "Insert the Python function template. The cursor must be on the first line
+of the function definition. Argument names are automatically filled in."
+  (interactive)
+  (save-excursion
+    (beginning-of-line)
+    (let* ((start-paren (save-excursion (re-search-forward "(")))
+           (end-paren (save-excursion (re-search-forward ")" )))
+           (the-colon (save-excursion (re-search-forward ":")))
+           (formals ()))
+      (goto-char start-paren)
+      (while (re-search-forward "\\([^,)]+\\)[,)]" end-paren t)
+        (let* ((entire-match  (match-string 1))
+               (formal-with-space (car 
+                                   (split-string (match-string 1)  "[ ,=]" t)))
+               (formal  (car (split-string formal-with-space))))
+          (setq formals (if (string= formal "self") formals
+                          (append  formals (list formal) )))))
+      (goto-char the-colon)
+      (insert "\n")
+      (indent-for-tab-command)
+      (insert "\"\"\"\n\n")
+      (when formals
+        (indent-for-tab-command)
+        (insert "Args:\n")
+        (indent-for-tab-command)
+        (dolist (formal formals)
+          (indent-for-tab-command)
+          (insert (concat "    " formal ": \n")))
+        (insert "\n"))
+      (indent-for-tab-command)
+      (insert "Returns:\n")
+      (indent-for-tab-command)
+      (insert "    \n")
+      (indent-for-tab-command)
+      (insert "\"\"\"\n"))))
 
